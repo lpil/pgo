e3dd2da734c1219f267413026bb9c4edc2cd7f5a
Guillaume Hivert <hivert.is.coming@gmail.com>
2024-05-04 15:27:22 +0200

    Reenables test on CI

--- test/reset_db.sh
+++ test/reset_db.sh
@@ -5,14 +5,14 @@ set -eu
 echo
 echo Resetting database
 
-psql -h localhost -U postgres <<SQL
+psql <<SQL
 SELECT 'CREATE DATABASE gleam_pgo_test'
 WHERE NOT EXISTS (
   SELECT FROM pg_database WHERE datname = 'gleam_pgo_test'
 )\\gexec
 SQL
 
-psql -v "ON_ERROR_STOP=1" -h localhost -U postgres -d gleam_pgo_test <<SQL
+psql -v "ON_ERROR_STOP=1" -d gleam_pgo_test <<SQL
 DROP TABLE IF EXISTS cats;
 CREATE TABLE cats (
   id SERIAL PRIMARY KEY,
